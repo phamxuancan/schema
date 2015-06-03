5aac7e186da2b004551c4c5e980dfb1c28e45be8
phamxuancan <dongsonglangle1990@gmail.com>
2015-06-03 10:48:33 +0700

    first

---
+++ app/Config/Schema/schema.php
@@ -0,0 +1,36 @@
+<?php
+class AppSchema extends CakeSchema {
+
+    public function before($event = array()) {
+        return true;
+    }
+
+    public function after($event = array()) {
+    }
+    var $comments = array(
+        'id' => array(
+            'type' => 'integer',
+            'null' => false,
+            'default' => 0,
+            'key' => 'primary'
+        ),
+        'post_id' => array('type' => 'integer', 'null' => false, 'default' => 0),
+        'comment' => array('type' => 'text'),
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id', 'unique' => true),
+            'post_id' => array('column' => 'post_id'),
+        ),
+        'tableParameters' => array(
+            'engine' => 'InnoDB',
+            'charset' => 'latin1',
+            'collate' => 'latin1_general_ci'
+        )
+    );
+    public $user = array(
+        'id'=>array(
+            'type'=>'integer',
+            'null'=>false,
+            'key'=>'primary',
+        )
+    );
+}
